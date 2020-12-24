c57bf08f66ff1cf354b8b337b1da3ca48eb333cf
anshul-birajdar <69183888+anshul-birajdar@users.noreply.github.com>
2020-12-24 14:39:12 +0530

    Update tty_retro_font.sh

--- tty_retro_font.sh
+++ tty_retro_font.sh
@@ -1,10 +1,3 @@
-setfont Lat15-Terminus32x16
-#setfont Lat38-VGA32x16.psf.gz
-#sleep 0.1
-#stty columns 80 rows 25
-#sleep 0.1
-#bash
-#setupcon -current-tty
-#sleep 0.1
+#this file adds retro font TO TTY i.e ctrl alt F1 to F6
 export PS1="\w% "
 setfont /usr/share/consolefonts/Lat7-VGA32x16.psf.gz
